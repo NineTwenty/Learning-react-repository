d888c858fc67ebdcd99bd4062c5e6e1a12544a27
NineTwenty <64965260+NineTwenty@users.noreply.github.com>
2020-05-09 20:38:13 +0400

    Add initial files

---
+++ src/components/Header.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default () => (<header className='app-header'>
+  <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRbqUkyAK84IcDm7zcaea_O8vPEYDDhEyZ-DR5wV_oqccAu65Zd&usqp=CAU'></img>
+</header>);
---
+++ src/components/Profile.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default () => (
+  <main className='app-main'>
+    <div>
+      <img src='https://imagekit.io/static/img/newPages/wave-bg.svg'></img>
+    </div>
+    <div>Profile</div>
+    <div>Posts</div>
+  </main>
+);
