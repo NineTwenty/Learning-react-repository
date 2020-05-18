ca9578484425bf979d5825eb42eda32f5961fa80
NineTwenty <64965260+NineTwenty@users.noreply.github.com>
2020-05-18 17:23:33 +0400

    Add Navbar  routing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
+import Dialogs from './components/Dialogs';
 import Profile from './components/Profile/Profile';
+import News from './components/News';
+import Music from './components/Music';
+import Settings from './components/Settings';
 
 function App() {
   return (
-    <div className='app-wrapper'>
-      <Header />
-      <Navbar />
-      <div className='app-wrapper-content'>
-        <Profile />
+    <BrowserRouter>
+      <div className='app-wrapper'>
+        <Header />
+        <Navbar />
+        <div className='app-wrapper-content'>
+          <Route path='/dialogs' component={Dialogs} />
+          <Route path='/profile' component={Profile} />
+          <Route path='/news' component={News} />
+          <Route path='/music' component={Music} />
+          <Route path='/settings' component={Settings} />
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
