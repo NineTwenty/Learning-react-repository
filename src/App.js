506cb69421d5b5f0ab8b3bc448a383afbecaf233
NineTwenty <64965260+NineTwenty@users.noreply.github.com>
2020-05-22 02:37:14 +0400

    Add COLORS css variables

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
+import './colors.css'
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Dialogs from './components/Dialogs/Dialogs';
