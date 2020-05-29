80b862378c7542fe374edfc4815ba83c74255189
NineTwenty <64965260+NineTwenty@users.noreply.github.com>
2020-05-29 18:55:58 +0400

    Transfer dummy data from component to index page

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,48 +3,13 @@ import s from './Dialogs.module.css';
 import Dialog from './Dialog/Dialog';
 import Chat from './Chat/Chat';
 
-const dialogsData = [
-  {
-    avatar: 'https://loremflickr.com/48/48?r=1',
-    name: 'Charles',
-    messages: `I'm stupid`,
-    count: 16,
-    time: '1min',
-    id: 1,
-  },
-  {
-    avatar: 'https://loremflickr.com/48/48?r=2',
-    name: 'Lando',
-    messages: `Its BWOKEN`,
-    count: 4,
-    time: '2min',
-    id: 2,
-  },
-  {
-    avatar: 'https://loremflickr.com/48/48?r=3',
-    name: 'Max',
-    messages: `What a f*****g idiot`,
-    count: 0,
-    time: '',
-    id: 3,
-  },
-  {
-    avatar: 'https://loremflickr.com/48/48?r=4',
-    name: 'Charles',
-    messages: `I'm stupid`,
-    count: 0,
-    time: '',
-    id: 4,
-  },
-];
-
 export default (props) => {
   return (
     <div className={s.dialogsWrapper}>
       <div className={s.dialogs}>
         <h2>Dialogs</h2>
-        {dialogsData.map((data) => (
-          <Dialog {...data} />
+        {props.data.map((data) => (
+          <Dialog key={data.id} {...data} />
         ))}
       </div>
       <Chat />
--- src/index.js
+++ src/index.js
@@ -4,9 +4,48 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const dialogsData = [
+  {
+    avatar: 'https://loremflickr.com/48/48?r=1',
+    name: 'Charles',
+    messages: `I'm stupid`,
+    count: 16,
+    time: '1min',
+    id: 1,
+  },
+  {
+    avatar: 'https://loremflickr.com/48/48?r=2',
+    name: 'Lando',
+    messages: `Its BWOKEN`,
+    count: 4,
+    time: '2min',
+    id: 2,
+  },
+  {
+    avatar: 'https://loremflickr.com/48/48?r=3',
+    name: 'Max',
+    messages: `What a f*****g idiot`,
+    count: 0,
+    time: '',
+    id: 3,
+  },
+  {
+    avatar: 'https://loremflickr.com/48/48?r=4',
+    name: 'Charles',
+    messages: `I'm stupid`,
+    count: 0,
+    time: '',
+    id: 4,
+  },
+];
+
+const appData = {
+  dialogsData,
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App {...appData} />
   </React.StrictMode>,
   document.getElementById('root')
 );
