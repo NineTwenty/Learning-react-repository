a1a87b416bf2736223a8aa8a5c959a30e65c0690
NineTwenty <64965260+NineTwenty@users.noreply.github.com>
2020-08-25 20:03:08 +0400

    [feature/Profile] Starting implementation of profile page

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
-import s from './Profile.module.css';
+import style from './Profile.module.css';
 import PostWall from './PostsWall/PostWall';
+import { Tabs } from '../common/Tabs';
+import { Tab } from '../common/Tab';
+import { Route, useRouteMatch } from 'react-router-dom';
 
-export default () => (
-  <main className={s.profile}>
-    <div>
-      <img src='https://imagekit.io/static/img/newPages/wave-bg.svg'></img>
-    </div>
-    <div>Profile img, description</div>
-    <PostWall />
-  </main>
-);
+const Profile = (props) => {
+  const { url } = useRouteMatch();
+  return (
+    <main className={style.grid}>
+      <div className={style.picture}></div>
+      <div className={style.tabs}>
+        <Tabs>
+          <Tab value={'Posts'} route={'posts'} />
+          <Tab value={'About'} route={'about'} />
+          <Tab value={'Friend'} route={'friend'} />
+          <Tab value={'Photos'} route={'photos'} />
+        </Tabs>
+      </div>
+      <Route path={`${url}/posts`}>
+        <div className={style.aside}></div>
+        <PostWall className={style.postwall} />
+      </Route>
+    </main>
+  );
+};
+
+export default Profile;
