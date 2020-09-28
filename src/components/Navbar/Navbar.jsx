7f6b2697813c5c01ebd96e0edf00630e9c3d3c3f
NineTwenty <64965260+NineTwenty@users.noreply.github.com>
2020-09-29 00:30:41 +0400

    Refactor Navbar css for slight more responsiveness

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,11 +5,21 @@ import { NavLink } from 'react-router-dom';
 export default () => {
   return (
     <nav className={s.nav}>
-      <NavLink to='/dialogs' activeClassName={s.active}>Dialogs</NavLink>
-      <NavLink to='/profile' activeClassName={s.active}>Profile</NavLink>
-      <NavLink to='/news' activeClassName={s.active}>News</NavLink>
-      <NavLink to='/music' activeClassName={s.active}>Music</NavLink>
-      <NavLink to="/settings" activeClassName={s.active}>Settings</NavLink>
+      <NavLink to='/dialogs' className={s.navLink} activeClassName={s.active}>
+        Dialogs
+      </NavLink>
+      <NavLink to='/profile' className={s.navLink} activeClassName={s.active}>
+        Profile
+      </NavLink>
+      <NavLink to='/news' className={s.navLink} activeClassName={s.active}>
+        News
+      </NavLink>
+      <NavLink to='/music' className={s.navLink} activeClassName={s.active}>
+        Music
+      </NavLink>
+      <NavLink to='/settings' className={s.navLink} activeClassName={s.active}>
+        Settings
+      </NavLink>
     </nav>
   );
 };
