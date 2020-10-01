ab2cc861a188d51ddd49cc16a2685e1704b14cc9
NineTwenty <64965260+NineTwenty@users.noreply.github.com>
2020-10-01 21:15:22 +0400

    Implement SideNav hide/open on menu button click

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,23 +1,45 @@
 import React from 'react';
-import s from './Navbar.module.css';
+import style from './Navbar.module.css';
 import { NavLink } from 'react-router-dom';
 
-export default () => {
+export default ({ isSideNavForceOpen }) => {
+  const isOpen = isSideNavForceOpen ? style.isOpen : '';
+
   return (
-    <nav className={s.nav}>
-      <NavLink to='/dialogs' className={s.navLink} activeClassName={s.active}>
+    <nav className={`${style.nav} ${isOpen}`}>
+      <NavLink
+        to='/dialogs'
+        className={style.navLink}
+        activeClassName={style.active}
+      >
         Dialogs
       </NavLink>
-      <NavLink to='/profile' className={s.navLink} activeClassName={s.active}>
+      <NavLink
+        to='/profile'
+        className={style.navLink}
+        activeClassName={style.active}
+      >
         Profile
       </NavLink>
-      <NavLink to='/news' className={s.navLink} activeClassName={s.active}>
+      <NavLink
+        to='/news'
+        className={style.navLink}
+        activeClassName={style.active}
+      >
         News
       </NavLink>
-      <NavLink to='/music' className={s.navLink} activeClassName={s.active}>
+      <NavLink
+        to='/music'
+        className={style.navLink}
+        activeClassName={style.active}
+      >
         Music
       </NavLink>
-      <NavLink to='/settings' className={s.navLink} activeClassName={s.active}>
+      <NavLink
+        to='/settings'
+        className={style.navLink}
+        activeClassName={style.active}
+      >
         Settings
       </NavLink>
     </nav>
