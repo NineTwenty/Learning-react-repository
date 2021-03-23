4bbce7566692802030dc38e585a2b0f93a504651
NineTwenty <64965260+NineTwenty@users.noreply.github.com>
2021-03-23 19:01:46 +0400

    Implement POST api method

--- src/api/API.js
+++ src/api/API.js
@@ -56,4 +56,14 @@ export const api = {
       return body;
     }
   },
+  /**
+   * @param {string} url
+   * @param {object} payload
+   */
+  async post(url, payload) {
+    const { ok, body } = await getAgent().post(url).send(payload);
+    if (ok) {
+      return body;
+    }
+  },
 };
