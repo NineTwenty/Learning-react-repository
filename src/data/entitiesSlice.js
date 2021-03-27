432ebd0a3439026cc84b4537a3ede46f4f9f56fc
NineTwenty <64965260+NineTwenty@users.noreply.github.com>
2021-03-27 15:23:25 +0400

    Implement usersSlice

--- src/data/entitiesSlice.js
+++ src/data/entitiesSlice.js
@@ -1,11 +1,13 @@
 import reducerRegistry from 'data/reducerRegistery';
 import { combineReducers } from 'redux';
 import { postsReducer } from './postSlice/postsSlice';
+import { usersReducer } from './usersSlice';
 // forced export to include module in webpack bundle
 export const workaround = 'workaround';
 const sliceName = 'entities';
 const entities = {
-  posts: postsReducer
+  posts: postsReducer,
+  users: usersReducer
 };
 
 const entitiesReducer = combineReducers(entities);
---
+++ src/data/usersSlice.js
@@ -0,0 +1,87 @@
+import { createEntityAdapter, createSlice } from '@reduxjs/toolkit';
+import { api } from 'api/API';
+import {
+  createLoadingActions,
+  createLoadingMatchers,
+  createLoadingReducers,
+} from 'data/utils';
+
+const sliceName = 'users';
+
+// Loading reducers
+const { handleRequestStart, handleRequestEnd } = createLoadingReducers();
+// Loading matchers
+const { isStartOfRequest, isEndOfRequest } = createLoadingMatchers(sliceName);
+
+// Adapter
+const adapter = createEntityAdapter();
+
+const initialState = adapter.getInitialState({ status: 'idle' });
+
+// Actions
+const getRequest = createLoadingActions(sliceName, 'get');
+const submitRequest = createLoadingActions(sliceName, 'submit');
+
+// Slice
+
+const usersSlice = createSlice({
+  name: sliceName,
+  initialState,
+  reducers: {
+    addOne: adapter.addOne,
+    addMany: adapter.addMany,
+    setAll: adapter.setAll,
+    removeOne: adapter.removeOne,
+    removeMany: adapter.removeMany,
+    updateOne: adapter.updateOne,
+    updateMany: adapter.updateMany,
+    upsertOne: adapter.upsertOne,
+    upsertMany: adapter.upsertMany,
+  },
+  extraReducers: (builder) => {
+    // setAll payload of 'success' get action
+    builder.addCase(getRequest.success.type, adapter.setAll);
+    builder.addCase(submitRequest.success.type, adapter.addOne);
+    builder.addMatcher(isStartOfRequest, handleRequestStart);
+    builder.addMatcher(isEndOfRequest, handleRequestEnd);
+  },
+});
+
+// Reducer
+
+export const usersReducer = usersSlice.reducer;
+
+// Exported actions
+
+export const addUsers = usersSlice.actions.addMany
+
+// Thunks
+
+export const fetchUsers = () => async (dispatch) => {
+  dispatch(getRequest.request());
+  try {
+    const { users } = await api.get('users');
+    dispatch(getRequest.success(users));
+  } catch (error) {
+    dispatch(getRequest.failure(error));
+  }
+};
+
+export const submitUser = (newUser) => async (dispatch) => {
+  dispatch(submitRequest.request());
+  try {
+    const { user } = await api.post('users', newUser);
+    dispatch(submitRequest.success(user));
+  } catch (error) {
+    dispatch(submitRequest.failure());
+  }
+};
+
+// Selectors
+
+const selectors = adapter.getSelectors((state) => state.entities[sliceName]);
+
+const { selectIds, selectById } = selectors;
+
+export const selectUsersIds = (state) => selectIds(state);
+export const selectUserById = (id) => (state) => selectById(state, id);
