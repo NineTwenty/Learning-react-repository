ee03bf7607a4cecf164c2dcfd91db847e9d803b3
NineTwenty <64965260+NineTwenty@users.noreply.github.com>
2021-03-27 22:17:18 +0400

    Implement dialogsSlice

---
+++ src/data/dialogsSlice.js
@@ -0,0 +1,90 @@
+import { createEntityAdapter, createSlice } from '@reduxjs/toolkit';
+import { api } from 'api/API';
+import {
+  createLoadingActions,
+  createLoadingMatchers,
+  createLoadingReducers,
+} from 'data/utils';
+import { addUsers } from './usersSlice';
+
+const sliceName = 'dialogs';
+
+// Loading reducers
+const { handleRequestStart, handleRequestEnd } = createLoadingReducers();
+// Loading matchers
+const { isStartOfRequest, isEndOfRequest } = createLoadingMatchers(sliceName);
+
+// Adapter
+const adapter = createEntityAdapter();
+
+const initialState = adapter.getInitialState({ status: 'idle' });
+
+// Loading actions
+const getRequest = createLoadingActions(sliceName, 'get');
+const submitRequest = createLoadingActions(sliceName, 'submit');
+
+// Slice
+
+const dialogsSlice = createSlice({
+  name: sliceName,
+  initialState,
+  reducers: {
+    addOne: adapter.addOne,
+    addMany: adapter.addMany,
+    setAll: adapter.setAll,
+    removeOne: adapter.removeOne,
+    removeMany: adapter.removeMany,
+    updateOne: adapter.updateOne,
+    updateMany: adapter.updateMany,
+    upsertOne: adapter.upsertOne,
+    upsertMany: adapter.upsertMany,
+  },
+  extraReducers: (builder) => {
+    // setAll payload of 'success' get action
+    builder.addCase(getRequest.success.type, adapter.setAll);
+    builder.addCase(submitRequest.success.type, adapter.addOne);
+    builder.addMatcher(isStartOfRequest, handleRequestStart);
+    builder.addMatcher(isEndOfRequest, handleRequestEnd);
+  },
+});
+
+// Reducer
+
+export const dialogsReducer = dialogsSlice.reducer;
+
+// Actions
+
+export const addDialogs = dialogsSlice.actions.addMany;
+
+// Thunks
+
+export const fetchDialogs = () => async (dispatch) => {
+  dispatch(getRequest.request());
+  try {
+    const { dialogs, users } = await api.get('dialogs?include=members');
+    dispatch(getRequest.success(dialogs));
+    dispatch(addUsers(users));
+  } catch (error) {
+    dispatch(getRequest.failure(error));
+  }
+};
+
+export const submitDialog = (newDialog) => async (dispatch) => {
+  dispatch(submitRequest.request());
+  try {
+    const { dialog } = await api.post('dialogs', newDialog);
+    dispatch(submitRequest.success(dialog));
+  } catch (error) {
+    dispatch(submitRequest.failure());
+  }
+};
+
+// Selectors
+
+const selectors = adapter.getSelectors((state) => state.entities[sliceName]);
+
+const { selectIds, selectById, selectAll } = selectors;
+
+export const selectDialogs = (state) => selectAll(state);
+export const selectDialogsIds = (state) => selectIds(state);
+export const selectDialogById = (id) => (state) => selectById(state, id);
--- src/data/entitiesSlice.js
+++ src/data/entitiesSlice.js
@@ -1,11 +1,13 @@
 import reducerRegistry from 'data/reducerRegistery';
 import { combineReducers } from 'redux';
+import { dialogsReducer } from './dialogsSlice';
 import { postsReducer } from './postSlice/postsSlice';
 import { usersReducer } from './usersSlice';
 // forced export to include module in webpack bundle
 export const workaround = 'workaround';
 const sliceName = 'entities';
 const entities = {
+  dialogs: dialogsReducer,
   posts: postsReducer,
   users: usersReducer
 };
