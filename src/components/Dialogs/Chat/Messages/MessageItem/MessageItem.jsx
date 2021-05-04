7e058b4958bcf0fab7d84821f8232801965c1af4
NineTwenty <64965260+NineTwenty@users.noreply.github.com>
2021-05-04 14:07:58 +0400

    Implement MessageItem orientation by author

--- src/components/Dialogs/Chat/Messages/MessageItem/MessageItem.jsx
+++ src/components/Dialogs/Chat/Messages/MessageItem/MessageItem.jsx
@@ -1,18 +1,37 @@
 import React from 'react';
 import styles from './MessageItem.module.css';
 import Avatar from '../../../../Profile/Avatar/Avatar';
+import { useSelector } from 'react-redux';
+import { selectUserById } from 'redux/entities';
+import { Spinner } from 'components/common/Spinner';
 
-const MessageItem = ({ avatar, username, text}) => {
-  return (
-    <div className={styles.messageItemWrapper}>
-      <Avatar
-        className={styles.messageItemAvatar}
-        avatar={avatar}
-        name={username}
-      />
-      <span className={styles.messageItemText}>{text}</span>
-    </div>
-  );
+const MessageItem = ({ text, id, authorId, positionLeft }) => {
+  const wrapperClasses = `${styles.messageItemWrapper} ${
+    positionLeft ? styles.messageItemWrapper_positionLeft : ''
+  }`;
+
+  const author = useSelector(selectUserById(authorId));
+
+  if (author) {
+    const { avatar, username } = author;
+    return (
+      <div className={wrapperClasses}>
+        <Avatar
+          className={styles.messageItemAvatar}
+          avatar={avatar}
+          name={username}
+          key={id}
+        />
+        <span className={styles.messageItemText}>{text}</span>
+      </div>
+    );
+  } else {
+    return (
+      <div className={styles.messageItemWrapper}>
+        <Spinner />
+      </div>
+    );
+  }
 };
 
 export default MessageItem;
