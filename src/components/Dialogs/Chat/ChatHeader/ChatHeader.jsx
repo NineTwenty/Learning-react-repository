df862259a2df8dfd5dcd6df1ff623c23ae9edd22
NineTwenty <64965260+NineTwenty@users.noreply.github.com>
2021-05-18 16:03:18 +0400

    feat(ChatHeader): Finish base implementation

--- src/components/Dialogs/Chat/ChatHeader/ChatHeader.jsx
+++ src/components/Dialogs/Chat/ChatHeader/ChatHeader.jsx
@@ -1,8 +1,18 @@
+import Avatar from 'components/Profile/Avatar/Avatar';
 import React from 'react';
 import styles from './ChatHeader.module.css';
 
-const ChatHeader = ({ children }) => {
-  return <div className={styles.chatHeaderWrapper}>{children}</div>;
+const ChatHeader = ({ children, member }) => {
+  const { avatar, firstName, lastName } = member;
+  const fullname = `${firstName} ${lastName}`;
+
+  return (
+    <div className={styles.Wrapper}>
+      <div className={styles.ButtonBox}>{children}</div>
+      <h4 className={styles.MemberName}>{fullname}</h4>
+      <Avatar {...{ avatar, name: fullname }} />
+    </div>
+  );
 };
 
 export default ChatHeader;
