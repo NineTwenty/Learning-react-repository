26563d356da5dcb386a7db6e45255856c35ba800
NineTwenty <64965260+NineTwenty@users.noreply.github.com>
2021-05-20 20:22:20 +0400

    feat(ChatInputs): Add state for textarea value

--- src/components/Dialogs/Chat/ChatInputs/ChatInputs.jsx
+++ src/components/Dialogs/Chat/ChatInputs/ChatInputs.jsx
@@ -1,12 +1,25 @@
 import Button from 'components/common/Button';
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './ChatImputs.module.css';
 
 const ChatInputs = (props) => {
+  const [value, setValue] = useState('');
+
+  const onChange = (event) => {
+    setValue(event.target.value);
+  };
+
   return (
     <div className={styles.chatImputsWrapper}>
       <form action=''>
-        <textarea name='message' id='' cols='20' rows='3' />
+        <textarea
+          onChange={onChange}
+          value={value}
+          name='message'
+          id=''
+          cols='20'
+          rows='3'
+        />
         <Button type='submit'>Send</Button>
       </form>
     </div>
