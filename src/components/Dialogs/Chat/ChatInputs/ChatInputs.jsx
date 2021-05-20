ea83f8f81101979e6ee67498be16c1d18f015ffd
NineTwenty <64965260+NineTwenty@users.noreply.github.com>
2021-05-20 19:23:47 +0400

    refactor(ChatInputs): Remove unused code

--- src/components/Dialogs/Chat/ChatInputs/ChatInputs.jsx
+++ src/components/Dialogs/Chat/ChatInputs/ChatInputs.jsx
@@ -1,34 +1,15 @@
 import React from 'react';
 import styles from './ChatImputs.module.css';
-import { useParams } from 'react-router-dom';
 
 const ChatInputs = (props) => {
-  // { dispatch, textareaState: {text} }
-  // const { id } = useParams();
-
-
-  // function updateTextareaContent(event) {
-  //   const text = event.target.value;
-  //   const action = updateNewMessageTextCreator(text);
-  //   dispatch(action);
-  // }
-
-  // function addMessage() {
-  //   const action = addMessageCreator(+id, text);
-  //   dispatch(action);
-  // }
-
   return (
     <div className={styles.chatImputsWrapper}>
       <textarea
-        // onChange={updateTextareaContent}
         name='Message'
         id=''
         cols='20'
         rows='3'
-        // value={text}
       />
-      {/* <button onClick={addMessage}>Send</button> */}
     </div>
   );
 };
