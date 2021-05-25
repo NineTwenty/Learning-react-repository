d876ef8deb6957a1e62188c1854a812980464d4e
NineTwenty <64965260+NineTwenty@users.noreply.github.com>
2021-05-25 16:27:48 +0400

    refactor(Avatar): Add props destructuring

--- src/components/Profile/Avatar/Avatar.jsx
+++ src/components/Profile/Avatar/Avatar.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import styles from './Avatar.module.css';
 
-const Avatar = (props) => {
+const Avatar = ({ className, avatar, name }) => {
   return (
-    <div className={`${props.className} ${styles.Wrapper}`}>
-      <img className={styles.Img} src={props.avatar} alt={props.name} />
+    <div className={`${className} ${styles.Wrapper}`}>
+      <img className={styles.Img} src={avatar} alt={name} />
       {/* <span className={`${styles.Status} ${styles.online}`}>1</span> */}
     </div>
   );
