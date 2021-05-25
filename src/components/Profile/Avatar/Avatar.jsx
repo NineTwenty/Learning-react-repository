6a37ffe24e1ebb403bd7226468958c3639f07fe9
NineTwenty <64965260+NineTwenty@users.noreply.github.com>
2021-05-25 16:54:29 +0400

    fix(Avatar): Change Img class name to Image

--- src/components/Profile/Avatar/Avatar.jsx
+++ src/components/Profile/Avatar/Avatar.jsx
@@ -4,7 +4,7 @@ import styles from './Avatar.module.css';
 const Avatar = ({ className, avatar, name }) => {
   return (
     <div className={`${className} ${styles.Wrapper}`}>
-      <img className={styles.Img} src={avatar} alt={name} />
+      <img className={styles.Image} src={avatar} alt={name} />
       {/* <span className={`${styles.Status} ${styles.online}`}>1</span> */}
     </div>
   );
