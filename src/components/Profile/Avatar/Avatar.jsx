61c1b55f658b967aba2d1fd08ba4dd0a1d6feb0d
NineTwenty <64965260+NineTwenty@users.noreply.github.com>
2021-05-25 16:17:22 +0400

    refactor(Avatar): Change css classes naming

--- src/components/Profile/Avatar/Avatar.jsx
+++ src/components/Profile/Avatar/Avatar.jsx
@@ -3,9 +3,9 @@ import styles from './Avatar.module.css';
 
 const Avatar = (props) => {
   return (
-    <div className={`${props.className} ${styles.avatarWrapper}`}>
-      <img className={styles.avatarImg} src={props.avatar} alt={props.name} />
-      {/* <span className={`${styles.avatarStatus} ${styles.online}`}>1</span> */}
+    <div className={`${props.className} ${styles.Wrapper}`}>
+      <img className={styles.Img} src={props.avatar} alt={props.name} />
+      {/* <span className={`${styles.Status} ${styles.online}`}>1</span> */}
     </div>
   );
 };
