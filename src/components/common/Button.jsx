b9f3dbbcf1eb8951823e0e5842e7800808cc2dec
NineTwenty <64965260+NineTwenty@users.noreply.github.com>
2021-06-23 23:14:42 +0400

    refactor(Button): Change css to scss

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './Button.module.css';
+import styles from './Button.module.scss';
 
 const Button = ({ className, type, children, ...rest }, ref) => {
   return (
