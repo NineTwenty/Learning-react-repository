b0667dfd9d7d3013da8a11028bb598c61cdaaa5f
NineTwenty <64965260+NineTwenty@users.noreply.github.com>
2021-06-26 21:32:40 +0400

    refactor(Icon): Use inline svg

--- src/components/common/Icon/Icon.jsx
+++ src/components/common/Icon/Icon.jsx
@@ -1,25 +1,38 @@
 import React from 'react';
 import styles from './Icon.module.scss';
 
-const reqIcons = require.context('images', true, /\.svg$/);
+import { ReactComponent as chevronLeftBlack } from 'images/chevron_left_black.svg';
+import { ReactComponent as chevronRightBlack } from 'images/chevron_right_black.svg';
+import { ReactComponent as menuBlack } from 'images/menu_black.svg';
+import { ReactComponent as menuOpenBlack } from 'images/menu_open_black.svg';
+import { ReactComponent as chevronLeftWhite } from 'images/chevron_left_white.svg';
+import { ReactComponent as chevronRightWhite } from 'images/chevron_right_white.svg';
+import { ReactComponent as menuWhite } from 'images/menu_white.svg';
+import { ReactComponent as menuOpenWhite } from 'images/menu_open_white.svg';
 
 export const Icon = ({ color = 'black', type, className }) => {
   const iconsEnum = {
     black: {
-      chevronLeft: reqIcons('./chevron_left_black.svg').default,
-      chevronRight: reqIcons('./chevron_right_black.svg').default,
-      menu: reqIcons('./menu_black.svg').default,
-      menuOpen: reqIcons('./menu_open_black.svg').default,
+      chevronLeft: chevronLeftBlack,
+      chevronRight: chevronRightBlack,
+      menu: menuBlack,
+      menuOpen: menuOpenBlack,
     },
     white: {
-      chevronLeft: reqIcons('./chevron_left_white.svg').default,
-      chevronRight: reqIcons('./chevron_right_white.svg').default,
-      menu: reqIcons('./menu_white.svg').default,
-      menuOpen: reqIcons('./menu_open_white.svg').default,
+      chevronLeft: chevronLeftWhite,
+      chevronRight: chevronRightWhite,
+      menu: menuWhite,
+      menuOpen: menuOpenWhite,
     },
   };
 
-  const icon = iconsEnum[color][type];
+  const IconSvg = iconsEnum[color][type];
 
-  return <img className={`${styles.Icon} ${className}`} src={icon} alt='' aria-hidden={true} />;
+  return (
+    <IconSvg
+      title=''
+      className={`${styles.Icon} ${className}`}
+      aria-hidden={true}
+    />
+  );
 };
