b927b4a29167a8b35121a9f6242bc94484ef00ff
NineTwenty <64965260+NineTwenty@users.noreply.github.com>
2021-06-26 19:44:44 +0400

    feat(Button): Add light and borderless variant

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import styles from './Button.module.scss';
+import cx from 'classnames';
 
-const Button = ({ className, type, children, ...rest }, ref) => {
+const Button = ({ className, type, styleType, children, ...rest }, ref) => {
+  const classes = cx(styles.button, className, {
+    [styles.button_default]: styleType === undefined,
+    [styles.button_light]: styleType === 'light',
+    [styles.button_borderless]: styleType === 'borderless',
+  });
+
   return (
     <>
-      <button
-        className={`${styles.button} ${className}`}
-        type={type}
-        ref={ref}
-        {...rest}
-      >
+      <button className={classes} type={type} ref={ref} {...rest}>
         {children}
       </button>
     </>
