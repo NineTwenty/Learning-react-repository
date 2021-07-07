ffc00417265fabdb242c3e7b1db6e7be55a6eafb
NineTwenty <64965260+NineTwenty@users.noreply.github.com>
2021-07-07 19:28:10 +0400

    refactor(API): Use TypeScript

--- src/api/API.ts
+++ src/api/API.ts
@@ -1,22 +1,15 @@
 import { getAgent } from './APIUtils';
 
 export const api = {
-  /**
-   * Fetch with get method
-   * @param {string} url
-   */
-  async get(url) {
+  async get(url: string) {
     const { ok, body } = await getAgent().get(url);
 
     if (ok) {
       return body;
     }
   },
-  /**
-   * @param {string} url
-   * @param {object} payload
-   */
-  async post(url, payload) {
+
+  async post(url: string, payload: object) {
     const { ok, body } = await getAgent().post(url).send(payload);
     if (ok) {
       return body;
