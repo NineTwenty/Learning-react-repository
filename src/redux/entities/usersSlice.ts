9eaad2c930dd2d6135d326aa982beee510f7a973
NineTwenty <64965260+NineTwenty@users.noreply.github.com>
2021-07-08 19:37:03 +0400

    refactor(usersSlice): Use TypeScript

--- src/redux/entities/usersSlice.ts
+++ src/redux/entities/usersSlice.ts
@@ -1,26 +1,33 @@
-import { createEntityAdapter, createSlice } from '@reduxjs/toolkit';
+import { createEntityAdapter, createSlice, EntityId } from '@reduxjs/toolkit';
 import { api } from 'api/API';
+import { User } from 'common/entities.types';
 import {
   createLoadingActions,
   createLoadingMatchers,
   createLoadingReducers,
 } from 'redux/utils';
+import { StatusState } from 'redux/utils/utils.types';
 
 const sliceName = 'users';
 
+// Adapter
+const adapter = createEntityAdapter<User>();
+
+// Initial state
+const initialState = adapter.getInitialState<StatusState>({ status: 'idle' });
+
+// State type
+type UserState = typeof initialState;
+
 // Loading reducers
-const { handleRequestStart, handleRequestEnd } = createLoadingReducers();
+const { handleRequestStart, handleRequestEnd } =
+  createLoadingReducers<UserState>();
 // Loading matchers
 const { isStartOfRequest, isEndOfRequest } = createLoadingMatchers(sliceName);
 
-// Adapter
-const adapter = createEntityAdapter();
-
-const initialState = adapter.getInitialState({ status: 'idle' });
-
 // Actions
-const getRequest = createLoadingActions(sliceName, 'get');
-const submitRequest = createLoadingActions(sliceName, 'submit');
+const getRequest = createLoadingActions<User[]>(sliceName, 'get');
+const submitRequest = createLoadingActions<User>(sliceName, 'submit');
 
 // Slice
 
@@ -40,8 +47,8 @@ const usersSlice = createSlice({
   },
   extraReducers: (builder) => {
     // setAll payload of 'success' get action
-    builder.addCase(getRequest.success.type, adapter.setAll);
-    builder.addCase(submitRequest.success.type, adapter.addOne);
+    builder.addCase(getRequest.success, adapter.addMany);
+    builder.addCase(submitRequest.success, adapter.addOne);
     builder.addMatcher(isStartOfRequest, handleRequestStart);
     builder.addMatcher(isEndOfRequest, handleRequestEnd);
   },
@@ -51,25 +58,24 @@ const usersSlice = createSlice({
 export const usersReducer = usersSlice.reducer;
 export const usersSliceName = usersSlice.name;
 
-
 // Exported actions
 
-export const addUser = usersSlice.actions.addOne
-export const addUsers = usersSlice.actions.addMany
+export const addUser = usersSlice.actions.addOne;
+export const addUsers = usersSlice.actions.addMany;
 
 // Thunks
 
-export const fetchUsers = () => async (dispatch) => {
+export const fetchUsers = () => async (dispatch: any) => {
   dispatch(getRequest.request());
   try {
     const { users } = await api.get('users');
     dispatch(getRequest.success(users));
   } catch (error) {
-    dispatch(getRequest.failure(error));
+    dispatch(getRequest.failure());
   }
 };
 
-export const submitUser = (newUser) => async (dispatch) => {
+export const submitUser = (newUser: User) => async (dispatch: any) => {
   dispatch(submitRequest.request());
   try {
     const { user } = await api.post('users', newUser);
@@ -81,13 +87,16 @@ export const submitUser = (newUser) => async (dispatch) => {
 
 // Selectors
 
-const selectors = adapter.getSelectors((state) => state.entities[sliceName]);
+const { selectIds, selectById, selectEntities } = adapter.getSelectors(
+  (state: any): UserState => state.entities[sliceName]
+);
 
-const { selectIds, selectById, selectEntities } = selectors;
+export const selectUsersIds = (state: UserState) => selectIds(state);
 
-export const selectUsersIds = (state) => selectIds(state);
-export const selectUserById = (id) => (state) => selectById(state, id);
-export const selectUsersByIds = (ids) => (state) => {
+export const selectUserById = (id: EntityId) => (state: UserState) =>
+  selectById(state, id);
+
+export const selectUsersByIds = (ids: EntityId[]) => (state: UserState) => {
   const entities = selectEntities(state);
 
   return ids.map((id) => entities[id]).filter(Boolean);
