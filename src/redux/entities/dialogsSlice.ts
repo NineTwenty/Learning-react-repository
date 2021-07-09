12a1d1b65c58be05175df6da895581d3c1b31ecf
NineTwenty <64965260+NineTwenty@users.noreply.github.com>
2021-07-09 19:01:21 +0400

    refactor(dialogsSlice): Use TypeScript

--- src/redux/entities/dialogsSlice.ts
+++ src/redux/entities/dialogsSlice.ts
@@ -1,5 +1,5 @@
-// @ts-nocheck
-import { createEntityAdapter, createSlice } from '@reduxjs/toolkit';
+import { Dialog } from 'common/entities.types';
+import { createEntityAdapter, createSlice, EntityId } from '@reduxjs/toolkit';
 import { api } from 'api/API';
 import {
   createLoadingActions,
@@ -8,22 +8,30 @@ import {
 } from 'redux/utils';
 import { addUsers } from './usersSlice';
 import { selectCurrentUserId } from '..';
+import { AppDispatch, RootState } from 'redux/store';
+import { StatusState } from 'redux/utils/utils.types';
 
 const sliceName = 'dialogs';
 
+// Adapter
+const adapter = createEntityAdapter<Dialog>();
+
+// Initial state
+const initialState = adapter.getInitialState<StatusState>({ status: 'idle' });
+
+// State type
+type DialogsState = typeof initialState;
+
 // Loading reducers
-const { handleRequestStart, handleRequestEnd } = createLoadingReducers();
+const { handleRequestStart, handleRequestEnd } =
+  createLoadingReducers<DialogsState>();
+
 // Loading matchers
 const { isStartOfRequest, isEndOfRequest } = createLoadingMatchers(sliceName);
 
-// Adapter
-const adapter = createEntityAdapter();
-
-const initialState = adapter.getInitialState({ status: 'idle' });
-
 // Loading actions
-const getRequest = createLoadingActions(sliceName, 'get');
-const submitRequest = createLoadingActions(sliceName, 'submit');
+const getRequest = createLoadingActions<Dialog[]>(sliceName, 'get');
+const submitRequest = createLoadingActions<Dialog>(sliceName, 'submit');
 
 // Slice
 
@@ -60,45 +68,50 @@ export const addDialogs = dialogsSlice.actions.addMany;
 
 // Thunks
 
-export const fetchDialogs = () => async (dispatch) => {
+export const fetchDialogs = () => async (dispatch: AppDispatch) => {
   dispatch(getRequest.request());
   try {
     const { dialogs, users } = await api.get('dialogs?include=members');
     dispatch(getRequest.success(dialogs));
     dispatch(addUsers(users));
   } catch (error) {
-    dispatch(getRequest.failure(error));
+    dispatch(getRequest.failure());
   }
 };
 
-export const submitDialog = (newDialog) => async (dispatch) => {
-  dispatch(submitRequest.request());
-  try {
-    const { dialog } = await api.post('dialogs', newDialog);
-    dispatch(submitRequest.success(dialog));
-  } catch (error) {
-    dispatch(submitRequest.failure());
-  }
-};
+export const submitDialog =
+  (newDialog: Dialog) => async (dispatch: AppDispatch) => {
+    dispatch(submitRequest.request());
+    try {
+      const { dialog } = await api.post('dialogs', newDialog);
+      dispatch(submitRequest.success(dialog));
+    } catch (error) {
+      dispatch(submitRequest.failure());
+    }
+  };
 
 // Selectors
 
-const selectors = adapter.getSelectors((state) => state.entities[sliceName]);
+const selectors = adapter.getSelectors(
+  (state: RootState) => state.entities[sliceName]
+);
 
 const { selectIds, selectById, selectAll } = selectors;
 
-export const selectDialogs = (state) => selectAll(state);
-export const selectDialogsIds = (state) => selectIds(state);
-export const selectDialogById = (id) => (state) => selectById(state, id);
-export const selectDialogMemberId = (dialogId) => (state) => {
-  // Get current user
-  const userId = selectCurrentUserId(state);
-  // Get dialog
-  const dialog = selectDialogById(dialogId)(state);
-  // If dialog defined find member which is not current user
-  const memberId = dialog
-    ? dialog.members.find((member) => member !== userId)
-    : undefined;
+export const selectDialogs = (state: RootState) => selectAll(state);
+export const selectDialogsIds = (state: RootState) => selectIds(state);
+export const selectDialogById = (id: EntityId) => (state: RootState) =>
+  selectById(state, id);
+export const selectDialogMemberId =
+  (dialogId: EntityId) => (state: RootState) => {
+    // Get current user
+    const userId = selectCurrentUserId(state);
+    // Get dialog
+    const dialog = selectDialogById(dialogId)(state);
+    // If dialog defined find member which is not current user
+    const memberId = dialog
+      ? dialog.members.find((member) => member !== userId)
+      : undefined;
 
-  return memberId;
-};
+    return memberId;
+  };
