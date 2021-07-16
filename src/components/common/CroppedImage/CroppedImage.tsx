a72b13cb10cbd965031902351047db5dad433079
NineTwenty <64965260+NineTwenty@users.noreply.github.com>
2021-07-16 17:03:13 +0400

    feat(CroppedImage): Implement image open on click

--- src/components/common/CroppedImage/CroppedImage.tsx
+++ src/components/common/CroppedImage/CroppedImage.tsx
@@ -1,5 +1,8 @@
+import { useState, useCallback } from 'react';
 import styles from './CroppedImage.module.scss';
 import cx from 'classnames';
+import { Lightbox } from '../Lightbox/Lightbox';
+import { Image } from '../Image/Image';
 
 type ImageProps = {
   src: string;
@@ -18,14 +21,25 @@ export const CroppedImage = ({
   height,
   className,
 }: ImageProps) => {
+  const [isOpen, setIsOpen] = useState(false);
   const classes = cx(styles.Wrapper, { className });
 
+  const onClick = () => setIsOpen(true);
+  const onClose = useCallback(() => setIsOpen(false), []);
+
   return (
-    <div className={classes}>
-      <img
-        className={styles.Image}
-        {...{ src, srcSet, alt, width, height }}
-      />
-    </div>
+    <>
+      <div className={classes} onClick={onClick}>
+        <img
+          className={styles.Image}
+          {...{ src, srcSet, alt, width, height }}
+        />
+      </div>
+      {isOpen && (
+        <Lightbox onClose={onClose}>
+          <Image {...{ src, alt }} />
+        </Lightbox>
+      )}
+    </>
   );
 };
