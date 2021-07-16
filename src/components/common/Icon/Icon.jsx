4a64f83c28c54c88d404a173e3ad64a901644e2d
NineTwenty <64965260+NineTwenty@users.noreply.github.com>
2021-07-16 17:00:13 +0400

    feat(Icon): Add close variant

--- src/components/common/Icon/Icon.jsx
+++ src/components/common/Icon/Icon.jsx
@@ -9,20 +9,24 @@ import { ReactComponent as chevronLeftWhite } from 'images/chevron_left_white.sv
 import { ReactComponent as chevronRightWhite } from 'images/chevron_right_white.svg';
 import { ReactComponent as menuWhite } from 'images/menu_white.svg';
 import { ReactComponent as menuOpenWhite } from 'images/menu_open_white.svg';
+import { ReactComponent as closeBlack } from 'images/close_black.svg';
+import { ReactComponent as closeWhite } from 'images/close_white.svg';
 
-export const Icon = ({ color = 'black', type, className }) => {
+export const Icon = ({ color = 'black', type, className = '' }) => {
   const iconsEnum = {
     black: {
       chevronLeft: chevronLeftBlack,
       chevronRight: chevronRightBlack,
       menu: menuBlack,
       menuOpen: menuOpenBlack,
+      close: closeBlack,
     },
     white: {
       chevronLeft: chevronLeftWhite,
       chevronRight: chevronRightWhite,
       menu: menuWhite,
       menuOpen: menuOpenWhite,
+      close: closeWhite,
     },
   };
 
