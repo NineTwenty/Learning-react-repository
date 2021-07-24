e8da142a9c0cd93e8488413c95ec53596d113f62
NineTwenty <64965260+NineTwenty@users.noreply.github.com>
2021-07-24 20:24:47 +0400

    fix(Tab): Adapt to new routing

--- src/components/common/Tab.jsx
+++ src/components/common/Tab.jsx
@@ -1,18 +1,10 @@
 import React from 'react';
 import style from './Tab.module.css';
-import { NavLink, useRouteMatch } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 export const Tab = ({ value, route }) => {
-  const { url } = useRouteMatch();
-  // Always will be active if route was unprovided
-  const validRoute = route ? `${url}/${route}` : `${url}`;
-
   return (
-    <NavLink
-      to={validRoute}
-      className={style.link}
-      activeClassName={style.active}
-    >
+    <NavLink to={route} className={style.link} activeClassName={style.active}>
       {value}
     </NavLink>
   );
