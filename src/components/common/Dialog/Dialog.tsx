783d0005e42f5d3518bc3ac6637c790443a585d2
NineTwenty <64965260+NineTwenty@users.noreply.github.com>
2021-08-21 19:22:48 +0400

    feat(Dialog): Implement component

---
+++ src/components/common/Dialog/Dialog.tsx
@@ -0,0 +1,25 @@
+import cx from 'classnames';
+import styles from './Dialog.module.scss';
+import { Wrapper } from '../Wrapper/Wrapper';
+import { Portal } from '../Portal/Portal';
+import { Icon } from '../Icon/Icon';
+import Button from '../Button';
+
+interface DialogProps extends React.ComponentPropsWithoutRef<'div'> {
+  onClose: Function;
+}
+
+export const Dialog = ({ onClose, className, children, ...rest }: DialogProps) => {
+  const classes = cx(styles.Wrapper, className);
+
+  return (
+    <Portal>
+      <Wrapper className={classes} {...rest}>
+        <Button onClick={onClose} className={styles.Button} styleType='borderless'>
+          <Icon type='close' />
+        </Button>
+        {children}
+      </Wrapper>
+    </Portal>
+  );
+};
