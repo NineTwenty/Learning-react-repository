873b7fcf05e2371bda42cb8ab3d25e141b081797
NineTwenty <64965260+NineTwenty@users.noreply.github.com>
2021-12-22 22:27:57 +0400

    test(TextAreaField): Add base test case

---
+++ src/components/common/TextAreaField/TextAreaField.test.tsx
@@ -0,0 +1,40 @@
+import { render, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { Form, Formik, FormikHelpers } from 'formik';
+import SubmitField from 'components/common/SubmitField';
+import { TextAreaField } from './TextAreaField';
+
+it('Render and correctly integrate with form', async () => {
+  const initialValues = {
+    text: '',
+  };
+
+  const onSubmit = jest.fn(
+    (values, actions: FormikHelpers<typeof initialValues>) => {
+      actions.resetForm();
+    }
+  );
+
+  const { getByRole } = render(
+    <Formik initialValues={initialValues} onSubmit={onSubmit}>
+      <Form>
+        <TextAreaField name='text' />
+        <SubmitField buttonText='Post' />
+      </Form>
+    </Formik>
+  );
+
+  const string = 'Test string';
+  const textarea = getByRole('textbox');
+
+  userEvent.type(textarea, string);
+
+  expect(textarea).toHaveValue(string);
+
+  // Submit
+  userEvent.click(getByRole('button'));
+  await waitFor(() => expect(onSubmit).toBeCalledTimes(1));
+
+  // Assert that form reset work correctly
+  expect(textarea).toBeEmptyDOMElement();
+});
