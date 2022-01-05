ae7b7a7dc6134ba9aa0f421d8a55489dd2a10dd0
NineTwenty <64965260+NineTwenty@users.noreply.github.com>
2022-01-05 20:48:38 +0400

    feat: Add fixtures for mirage server

---
+++ src/api/fixtures/posts.ts
@@ -0,0 +1,252 @@
+export default [
+  {
+    authorId: '1',
+    feedId: '1',
+    views: 0,
+    postText:
+      'Numquam ab architecto. Enim labore id expedita beatae ab cupiditate iste vitae error. Eum neque amet deleniti eius vel nisi animi rerum. Sit debitis facilis quis autem aliquid animi itaque. Non qui et doloribus eum et.',
+    created: 1641396599022,
+    id: '1',
+  },
+  {
+    authorId: '1',
+    feedId: '1',
+    views: 0,
+    postText:
+      'Quas id sit sit qui. Est modi necessitatibus reiciendis velit ad accusamus nam.',
+    created: 1641396599022,
+    id: '2',
+  },
+  {
+    authorId: '2',
+    feedId: '2',
+    views: 0,
+    postText:
+      'Dicta fugiat et atque dolore dolor tempora nihil ea aspernatur. Quas enim repellendus doloremque eum maiores sit possimus aut fugit. Et autem voluptatibus autem consectetur consequuntur. Autem reiciendis iusto totam explicabo dolorem voluptatem et ut rerum. Consequuntur neque iste voluptatem magnam quia tempore eaque. Sint assumenda alias natus accusantium similique atque molestiae vero.',
+    created: 1641396599022,
+    id: '3',
+  },
+  {
+    authorId: '2',
+    feedId: '2',
+    views: 0,
+    postText:
+      'Laudantium ut voluptas accusamus et. Id non sed sunt perspiciatis sed molestias perferendis. Maxime nemo quia voluptatibus incidunt accusantium. Et ducimus nesciunt error. Libero eligendi vel tempore quis ea. Totam eum voluptas repellendus voluptas quas.',
+    created: 1641396599022,
+    id: '4',
+  },
+  {
+    authorId: '3',
+    feedId: '3',
+    views: 0,
+    postText:
+      'Nemo est ut sint omnis est eos deserunt. Perspiciatis amet nisi adipisci porro est tempora. Quia ut ut saepe quis ullam. Rerum in quos est laborum possimus quia unde et. Nam dolor similique. Ipsum ipsa omnis necessitatibus.',
+    created: 1641396599022,
+    id: '5',
+  },
+  {
+    authorId: '3',
+    feedId: '3',
+    views: 0,
+    postText:
+      'Quia ea voluptatibus voluptatum. Consectetur occaecati illo facere fugiat provident aut et veniam itaque.',
+    created: 1641396599022,
+    id: '6',
+  },
+  {
+    authorId: '4',
+    feedId: '4',
+    views: 0,
+    postText:
+      'Et quis sint. Illum culpa possimus vel pariatur architecto reprehenderit debitis.',
+    created: 1641396599022,
+    id: '7',
+  },
+  {
+    authorId: '4',
+    feedId: '4',
+    views: 0,
+    postText:
+      'Natus voluptatem nihil. Consequuntur ut minima et et mollitia eaque necessitatibus et. Sit quidem eligendi. Qui illum qui non.',
+    created: 1641396599022,
+    id: '8',
+  },
+  {
+    authorId: '5',
+    feedId: '5',
+    views: 0,
+    postText:
+      'Aspernatur et necessitatibus dignissimos aut aspernatur voluptas. Error non earum.',
+    created: 1641396599022,
+    id: '9',
+  },
+  {
+    authorId: '5',
+    feedId: '5',
+    views: 0,
+    postText:
+      'Consequatur voluptas qui quia ducimus. Impedit reiciendis officia consequatur. Voluptatem consequatur corporis consequatur sapiente. Corporis commodi velit ut laboriosam doloremque.',
+    created: 1641396599022,
+    id: '10',
+  },
+  {
+    authorId: '6',
+    feedId: '6',
+    views: 0,
+    postText:
+      'Unde cumque commodi vero. Maiores similique adipisci. Iure est dignissimos mollitia illo quia expedita. Incidunt vero porro dolores alias. Ab sed et.',
+    created: 1641396599022,
+    id: '11',
+  },
+  {
+    authorId: '6',
+    feedId: '6',
+    views: 0,
+    postText:
+      'Aut quos accusamus repellendus cum rerum qui aliquid sed. Nihil sed dolorem fuga porro.',
+    created: 1641396599022,
+    id: '12',
+  },
+  {
+    authorId: '7',
+    feedId: '7',
+    views: 0,
+    postText: 'Omnis at omnis et. Hic omnis incidunt ut.',
+    created: 1641396599022,
+    id: '13',
+  },
+  {
+    authorId: '7',
+    feedId: '7',
+    views: 0,
+    postText:
+      'Odit voluptatem qui voluptatem aut ipsam. Cupiditate et rem laudantium aut quas. Mollitia voluptatem culpa itaque non qui.',
+    created: 1641396599022,
+    id: '14',
+  },
+  {
+    authorId: '8',
+    feedId: '8',
+    views: 0,
+    postText:
+      'Tenetur laudantium et quisquam non animi. Esse nostrum consequuntur similique voluptatem suscipit debitis minus est quam. Quis molestiae necessitatibus deleniti repellat delectus sequi eos quisquam. Odit dolor aut laborum. Quaerat aut quae porro vitae laborum asperiores iste.',
+    created: 1641396599022,
+    id: '15',
+  },
+  {
+    authorId: '8',
+    feedId: '8',
+    views: 0,
+    postText: 'Iusto quo quas. Enim est ut blanditiis quibusdam quam ea.',
+    created: 1641396599022,
+    id: '16',
+  },
+  {
+    authorId: '9',
+    feedId: '9',
+    views: 0,
+    postText:
+      'Sit eveniet sit rerum. Rerum ipsam et cumque cumque aut esse sit.',
+    created: 1641396599022,
+    id: '17',
+  },
+  {
+    authorId: '9',
+    feedId: '9',
+    views: 0,
+    postText:
+      'Animi adipisci natus est. Et corporis nobis adipisci blanditiis sequi assumenda illum sunt. Perferendis ea deserunt molestiae rem. Ad iusto saepe consequatur eum ea ut ducimus doloremque. Dolore fuga optio quis.',
+    created: 1641396599022,
+    id: '18',
+  },
+  {
+    authorId: '10',
+    feedId: '10',
+    views: 0,
+    postText:
+      'Voluptatem assumenda veniam dolore omnis. Corporis perspiciatis consequuntur exercitationem est aut nihil quisquam. Quo vitae corrupti quod sed est nostrum consequatur labore nemo. Id sunt rerum expedita fugiat. Sint sit accusamus commodi tempore.',
+    created: 1641396599022,
+    id: '19',
+  },
+  {
+    authorId: '10',
+    feedId: '10',
+    views: 0,
+    postText:
+      'Rerum voluptatem accusamus. Minima enim rem laudantium voluptatem. Molestiae ut necessitatibus est.',
+    created: 1641396599022,
+    id: '20',
+  },
+  {
+    authorId: '11',
+    feedId: '11',
+    views: 0,
+    postText:
+      'Sit dolores eos rerum optio dicta delectus maxime odit praesentium. Facere sit saepe. Provident ipsa ullam in voluptatum repellendus tempore quis. Voluptatem sint odio quibusdam dolor rem eaque.',
+    created: 1641396599022,
+    id: '21',
+  },
+  {
+    authorId: '11',
+    feedId: '11',
+    views: 0,
+    postText:
+      'Eveniet aut sit ex. Officia aspernatur aut quod odit odio vero est doloribus nulla. Praesentium officia dolores natus facilis.',
+    created: 1641396599022,
+    id: '22',
+  },
+  {
+    authorId: '12',
+    feedId: '12',
+    views: 0,
+    postText:
+      'Qui rerum quod. Hic quo quia atque mollitia. Sed beatae accusantium ratione consequuntur possimus.',
+    created: 1641396599022,
+    id: '23',
+  },
+  {
+    authorId: '12',
+    feedId: '12',
+    views: 0,
+    postText:
+      'Non nihil aut voluptate nesciunt velit fugiat mollitia atque. Suscipit occaecati id. Harum quam consectetur voluptates accusamus dignissimos tenetur. Quidem ullam sequi non culpa rerum et quidem numquam.',
+    created: 1641396599022,
+    id: '24',
+  },
+  {
+    authorId: '13',
+    feedId: '13',
+    views: 0,
+    postText:
+      'Nihil consequatur qui. Mollitia nemo corrupti voluptatum nulla rerum.',
+    created: 1641396599022,
+    id: '25',
+  },
+  {
+    authorId: '13',
+    feedId: '13',
+    views: 0,
+    postText:
+      'Quia voluptas illo dolorem dolores. Possimus repudiandae dolor. Reiciendis consequatur tenetur. Voluptatem nobis recusandae dicta illum et alias modi quos.',
+    created: 1641396599022,
+    id: '26',
+  },
+  {
+    authorId: '14',
+    feedId: '14',
+    views: 0,
+    postText:
+      'Consequatur officiis maiores aut totam. Veniam nisi omnis veritatis et nobis laudantium.',
+    created: 1641396599022,
+    id: '27',
+  },
+  {
+    authorId: '14',
+    feedId: '14',
+    views: 0,
+    postText:
+      'Aut est vitae reprehenderit qui architecto quia adipisci quasi. Sint est dolorem. Omnis in aut nihil quidem et omnis.',
+    created: 1641396599022,
+    id: '28',
+  },
+];
