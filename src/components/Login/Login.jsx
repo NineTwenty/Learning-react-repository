8d9e41c50b3b6a068cba59f0cf9026f052358f2d
NineTwenty <64965260+NineTwenty@users.noreply.github.com>
2022-02-22 15:23:08 +0400

    feat: Add SignUpModalForm to Login page

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,4 +1,8 @@
+import { useState } from 'react';
 import { Redirect, useLocation } from 'react-router-dom';
+import Button from 'components/common/Button';
+import { Separator } from 'components/common/Separator';
+import SignUpModalForm from 'components/Login/SignUpModalForm';
 import style from './Login.module.scss';
 import LoginForm from './LoginForm';
 
@@ -6,6 +10,8 @@ function Login({ loggedIn }) {
   // Get redirected page path
   const location = useLocation();
   const { referrer } = location.state || { referrer: { pathname: '/profile' } };
+
+  const [isOpen, setIsOpen] = useState(false);
 
   // Redirect if user already logged in
   if (loggedIn) return <Redirect to={referrer} />;
@@ -17,6 +23,15 @@ function Login({ loggedIn }) {
         <h1>Sign in</h1>
         <p>Enter your login or email address and password to sign in.</p>
         <LoginForm loggedIn={loggedIn} />
+        <Separator className={style.Separator} />
+        <h2>Don&#39;t have an account?</h2>
+        <Button
+          styleType='light'
+          onClick={() => setIsOpen((currentIsOpen) => !currentIsOpen)}
+        >
+          Sign Up
+        </Button>
+        <SignUpModalForm isOpen={isOpen} />
       </div>
     </div>
   );
