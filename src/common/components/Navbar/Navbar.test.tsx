1aa10b03b667fefa9532fe67e68690afb62deb21
NineTwenty <64965260+NineTwenty@users.noreply.github.com>
2022-08-03 19:42:51 +0400

    test: Add Navbar tests

---
+++ src/common/components/Navbar/Navbar.test.tsx
@@ -0,0 +1,56 @@
+import userEvent from '@testing-library/user-event';
+import { createRef } from 'react';
+import { render, screen } from 'utils/test-utils';
+import { Navbar } from './Navbar';
+
+const renderNavbar = ({
+  btnRef = createRef<HTMLButtonElement>(),
+  setSideNavForceOpen = jest.fn(),
+  children = (
+    <Navbar
+      isSideNavForceOpen
+      setSideNavForceOpen={setSideNavForceOpen}
+      menuBtnRef={btnRef}
+    />
+  ),
+} = {}) => {
+  return render(children);
+};
+
+it('Change location on link click', async () => {
+  const { history } = renderNavbar();
+  expect(history.location.pathname).toBe('/');
+
+  const navBtn = screen.getByRole('link', { name: /profile/i });
+  userEvent.click(navBtn);
+
+  expect(history.location.pathname).toBe('/profile');
+});
+
+it('Turn off forced open on click outside', () => {
+  const btnRef = createRef<HTMLButtonElement>();
+  const setForceOpen = jest.fn();
+  renderNavbar({
+    children: (
+      <div>
+        <button type='button' ref={btnRef}>
+          menu
+        </button>
+        <Navbar
+          isSideNavForceOpen
+          setSideNavForceOpen={setForceOpen}
+          menuBtnRef={btnRef}
+        />
+        <div>outside</div>
+      </div>
+    ),
+  });
+
+  // Click on associated menu button shouldn't trigger callback
+  // cause they suppose to change same state
+  userEvent.click(screen.getByRole('button', { name: 'menu' }));
+  expect(setForceOpen).not.toBeCalled();
+
+  userEvent.click(screen.getByText('outside'));
+  expect(setForceOpen).toBeCalledWith(false);
+});
