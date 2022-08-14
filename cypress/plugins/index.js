09a4e49245f997b4d9718b7be74e5d054865e033
NineTwenty <64965260+NineTwenty@users.noreply.github.com>
2022-08-14 20:52:26 +0400

    chore: Setup global code coverage

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -19,4 +19,6 @@
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
+  // eslint-disable-next-line import/no-extraneous-dependencies, global-require
+  return require('@cypress/code-coverage/task')(on, config);
 };
