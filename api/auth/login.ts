a786c1f477ac7f743530922f06cd9a7784d2a9fb
NineTwenty <64965260+NineTwenty@users.noreply.github.com>
2022-10-29 17:41:12 +0400

    feat: Add /login api endpoint

---
+++ api/auth/login.ts
@@ -0,0 +1,28 @@
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+import { PrismaClient } from '@prisma/client';
+import { z } from 'zod';
+import { UnsecuredJWT } from 'jose';
+
+const prisma = new PrismaClient();
+const LoginRequestBody = z.object({
+  login: z.string(),
+  password: z.string(),
+});
+
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+  const validatedBody = LoginRequestBody.safeParse(req.body);
+  if (!validatedBody.success) {
+    return res.status(400);
+  }
+  const { login, password } = validatedBody.data;
+  const user = await prisma.user.findUnique({ where: { login } });
+  // Authentication
+  if (user && user.password === password) {
+    const token = new UnsecuredJWT({ userId: user.id })
+      .setIssuedAt()
+      .setExpirationTime('100m')
+      .encode();
+    return res.status(200).send({ token });
+  }
+  return res.status(401).send(['Wrong login or password']);
+}
