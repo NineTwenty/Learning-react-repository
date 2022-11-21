93a0af586d5b239554b7b834d9cbab03da6dffdf
NineTwenty <64965260+NineTwenty@users.noreply.github.com>
2022-11-21 19:15:34 +0400

    chore: Remove obsolete CRA related files

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,6 @@ import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import store from 'data/store';
 import { IconContext } from 'react-icons';
-import * as serviceWorker from './serviceWorker';
 
 import App from './app/App';
 
@@ -26,8 +25,3 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
