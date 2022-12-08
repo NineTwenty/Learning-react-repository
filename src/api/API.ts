5ea73e475973c8dea22d8166e68186c241a327ca
NineTwenty <64965260+NineTwenty@users.noreply.github.com>
2022-12-08 13:29:16 +0400

    refactor: Instantiate axios once

--- src/api/API.ts
+++ src/api/API.ts
@@ -1,18 +1,20 @@
 import { getInstance } from './APIUtils';
 
+const apiInstance = getInstance();
+
 export const api = {
   async get<T>(url: string) {
-    const res = await getInstance().get<T>(url);
+    const res = await apiInstance.get<T>(url);
     return res.data;
   },
 
   async post<T>(url: string, payload: Record<string, unknown>) {
-    const res = await getInstance().post<T>(url, payload);
+    const res = await apiInstance.post<T>(url, payload);
     return res.data;
   },
 
   async delete<T>(url: string) {
-    const res = await getInstance().delete<T>(url);
+    const res = await apiInstance.delete<T>(url);
 
     return res.data;
   },
